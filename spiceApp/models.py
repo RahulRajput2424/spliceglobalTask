c31cfe3641efef499607d39afe0b714bd88e9766
rahulrajput <rajputrahul24@live.com>
2020-10-30 00:36:01 +0530

    Add user type vendor and bidder in model

--- spiceApp/models.py
+++ spiceApp/models.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    USER_TYPE_CHOICES = (
+      (1, 'Vendor'),
+      (2, 'Bidder'),
+  )
+    user_type = models.IntegerField(choices=USER_TYPE_CHOICES,default=None)
     username = models.CharField(max_length=255,unique=True)
     email = models.EmailField(db_index=True, unique=True)
     mobileNumber = models.CharField(null=True,max_length=18, unique=True)
