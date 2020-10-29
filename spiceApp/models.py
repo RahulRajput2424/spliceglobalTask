b0c2a6b8ba7cbc7a2bfe9369a0a6ad18b5f1f947
rahulrajput <rajputrahul24@live.com>
2020-10-30 00:18:01 +0530

    Assignment base setup  with custom user model

---
+++ spiceApp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=255,unique=True)
+    email = models.EmailField(db_index=True, unique=True)
+    mobileNumber = models.CharField(null=True,max_length=18, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
